a1eb229747a92fc3cf4eb25e027140c7d0534cd3
artmizu <hello@artmizu.ru>
2022-12-07 00:18:04 +0400

    tests

--- test/getFormData.spec.ts
+++ test/getFormData.spec.ts
@@ -13,6 +13,7 @@ it('check getFormData', () => {
   vi.stubGlobal('File', class File { })
 
   const obj = {
+    a: true,
     x: 10,
     y: 'somevalue',
     z: ['1', '2'],
@@ -22,5 +23,5 @@ it('check getFormData', () => {
   }
 
   getFormData(obj)
-  expect(appendFn.mock.calls).toEqual([['x', '10'], ['y', 'somevalue'], ['z', '1'], ['z', '2'], ['t', '{"z":1,"x":2}'], ['t', '{"z":10,"w":20}']])
+  expect(appendFn.mock.calls).toEqual([['a', 'true'], ['x', '10'], ['y', 'somevalue'], ['z', '1'], ['z', '2'], ['t', '{"z":1,"x":2}'], ['t', '{"z":10,"w":20}']])
 })
